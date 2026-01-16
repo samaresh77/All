94a988281034d75fc44f626aa701c63699117968
Samaresh <118683079+samaresh77@users.noreply.github.com>
2026-01-16 18:12:04 +0000

    ex1

--- Python/Practice/abc/a.py
+++ Python/Practice/abc/a.py
@@ -1,1 +1,16 @@
-print("Hello, World!")
+# print("Hello, World!")
+
+# factorial of a number
+# def factorial(num) :
+#     if(num == 0 or num == 1) :
+#         return 1
+#     return num*factorial(num-1)
+# num = int(input("Enter a number: "))
+# print(f"factorial of {num} is {factorial(num)}")
+
+# Reverse a string
+def reverse(str) :
+    str = str[::-1]
+    return str
+str = input("Enter a string: ")
+print(reverse(str))
