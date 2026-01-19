14dbc2dd05a1ade59e1700392f1b5b4e34658424
Samaresh <118683079+samaresh77@users.noreply.github.com>
2026-01-19 04:40:25 +0000

    pdw gen

--- Python/Practice/abc/a.py
+++ Python/Practice/abc/a.py
@@ -9,8 +9,25 @@
 # print(f"factorial of {num} is {factorial(num)}")
 
 # Reverse a string
-def reverse(str) :
-    str = str[::-1]
-    return str
-str = input("Enter a string: ")
-print(reverse(str))
+# def reverse(str) :
+#     str = str[::-1]
+#     return str
+# str = input("Enter a string: ")
+# print(reverse(str))
+
+# generate a random password
+import random
+import string
+
+# Ask user for password length
+length = int(input("Enter password length: "))
+
+# Characters to use
+characters = string.ascii_letters + string.digits + string.punctuation
+
+# Generate password
+password = ""
+for i in range(length):
+    password += random.choice(characters)
+
+print("Generated Password:", password)
