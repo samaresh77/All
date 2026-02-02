f70644146445c1f1ce93aa0252f3c531ff4b2392
Samaresh <118683079+samaresh77@users.noreply.github.com>
2026-02-02 18:29:54 +0000

    cal2

---
+++ PythonNext/Week1/Smart-Calculator/cal2.py
@@ -0,0 +1,23 @@
+print("Smart Calculator is running!")
+
+def calculator():
+    num1 = int(input("Enter First number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = int(input("Enter Second number: "))
+
+    if operator == '+':
+        print(f"{num1} + {num2} = {num1 + num2}")
+    elif operator == '-':
+        print(f"{num1} - {num2} = {num1 - num2}")
+    elif operator == '*':
+        print(f"{num1} * {num2} = {num1 * num2}")
+    elif operator == '/':
+        if num2 != 0:
+            print(f"{num1} / {num2} = {num1 / num2}")
+        else:
+            print("Error: Division by zero is not allowed.")
+    else:
+        print("Error: Invalid operator.")
+
+if __name__ == "__main__":
+    calculator()
