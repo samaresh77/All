0dc5e269d9dc784004ca9509f389880a33ac5315
Samaresh <118683079+samaresh77@users.noreply.github.com>
2026-02-12 19:27:43 +0000

    node update

--- Full-Stack/Demo1/node-backend/server.js
+++ Full-Stack/Demo1/node-backend/server.js
@@ -5,7 +5,15 @@ const swaggerJsDoc = require("swagger-jsdoc");
 const axios = require("axios");
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://redesigned-potato-g9x6r7q66gj29gvr-5173.app.github.dev"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const PORT = 5000;
