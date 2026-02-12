64bb6140016e56b7de7c544c942ac33d85016655
Samaresh <118683079+samaresh77@users.noreply.github.com>
2026-02-12 19:27:56 +0000

    python update

--- Full-Stack/Demo1/python-backend/main.py
+++ Full-Stack/Demo1/python-backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://redesigned-potato-g9x6r7q66gj29gvr-5173.app.github.dev"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   # or ["*"] for development
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/python")
 def read_root():
