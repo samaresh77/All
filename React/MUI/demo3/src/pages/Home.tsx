e9e1b09d90b984790ba7ef6167794cf7c35b6260
Samaresh <118683079+samaresh77@users.noreply.github.com>
2026-02-14 18:18:01 +0000

    home

---
+++ React/MUI/demo3/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div style={{backgroundColor: "blue", margin: 0, padding: 0}}>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page!</p>
+    </div>
+  );
+}
+
+export default Home;
