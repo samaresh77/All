b3b1f687d72b13f8df688b3194120cbbd5579189
samaresh77 <samareshmondal53547@gmail.com>
2026-06-09 22:58:59 +0530

    update

--- Python/Tut/Numpy/n1.py
+++ Python/Tut/Numpy/n1.py
@@ -3,12 +3,16 @@ import numpy as np
 # Creating Array
 arr = np.array([1, 2, 3, 4, 5]) #1D Array
 print(arr)
-print(arr.shape)
+print("shape of 1d array->",arr.shape)
+print(f"dimention of array-> {arr.ndim}")
+print(f"size of 1d array-> {arr.size}")
 
 #2D Array
 arr2 = np.array([[1, 2, 3],[4, 5, 6]])
 print(arr2)
-print(arr2.shape)
+print("shape of 2d array->",arr2.shape)
+print(f"dimention of array-> {arr2.ndim}")
+print(f"size of 2d array-> {arr2.size}")
 
 #3D Array
 arr3 = np.array([
@@ -16,4 +20,6 @@ arr3 = np.array([
     [[5,6],[7,8]]
 ])
 print(arr3)
-print(arr3.shape)
+print("shape of 3d array->",arr3.shape)
+print(f"dimention of array-> {arr3.ndim}")
+print(f"size of 3d array-> {arr3.size}")
