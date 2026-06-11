8f19da7d41ed452f6882f6af2641bfea50adc0f3
samaresh77 <samareshmondal53547@gmail.com>
2026-06-11 23:27:31 +0530

    u

--- Python/Tut/Panda/p2.py
+++ Python/Tut/Panda/p2.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv("/Student.csv")
-print(df)
+# Reading csv file
+df = pd.read_csv("/Users/samaresh/Personal/All/Python/Tut/Panda/Students.csv")
+# print(df) # Read CSV
+# print(df.head()) # View First 5 Rows
+# print(df.tail()) # View Last 5 Rows
+# print(df.info()) # Get Information About the Dataset
+print(df.describe()) # Get Statistical Summary
