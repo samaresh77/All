61304531981deb5ee6bd866ba03cb247dc7b074b
samaresh77 <samareshmondal53547@gmail.com>
2026-06-12 23:10:11 +0530

    a

--- Python/Tut/basic/a.py
+++ Python/Tut/basic/a.py
@@ -2,7 +2,8 @@
 from gtts import gTTS
 
 # Create a gTTS object with the desired text
-tts = gTTS(text="Hello Samaresh! Happy New Year", lang="en")
+# tts = gTTS(text="Hello Samaresh! Happy New Year", lang="en")
+tts = gTTS(text="Oi Rakhi! Phone ta rekhe dao nahale voy asbe ", lang="bn")
 
 # Save the generated speech to an mp3 file
 tts.save("hello.mp3")
